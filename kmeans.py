4216de55f690d664b04a944b65dbaf28eaf09f91
Artur <artur.pietrzak1@outlook.com>
2024-04-02 16:59:59 +0200

    Add second experiment

--- kmeans.py
+++ kmeans.py
@@ -1,15 +1,16 @@
 from main import load_data
 from sklearn.datasets import load_iris, load_wine, load_breast_cancer
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score, adjusted_rand_score, homogeneity_score, completeness_score, \
+    v_measure_score
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
-
 SCALE = 1.05
 COLORS = ['red', 'green', 'blue', 'purple', 'orange', 'pink', 'cyan', 'olive', 'brown', 'gray']
+n_clusters_max = 10
 
 
 def plot_voronoi_diagram(X, y_true, y_pred, subtitle=None):
@@ -83,11 +84,11 @@ def plot_silhouette_score(X, y):
     plt.show()
 
 
+# analysis for experiment one
 def analyze_kmeans_silhouette_score(X, y, dataset_name):
     scores = [0, 0]
-    n_clusters_max = 10
-    X = StandardScaler().fit_transform(X)
 
+    X = StandardScaler().fit_transform(X)
     best_score = -1
     best_score_idx = -1
     worst_score = 1
@@ -118,18 +119,16 @@ def analyze_kmeans_silhouette_score(X, y, dataset_name):
 
     if X.shape[1] == 2:
         kmeans = KMeans(n_clusters=best_score_idx, random_state=1, n_init="auto")
-        plot_voronoi_diagram(X, y, kmeans.fit_predict(X), f'{dataset_name} best score; n_clusters={best_score_idx}')
+        plot_voronoi_diagram(X, None, kmeans.fit_predict(X), f'{dataset_name} best score; n_clusters={best_score_idx}')
         kmeans = KMeans(n_clusters=worst_score_idx, random_state=1, n_init="auto")
-        plot_voronoi_diagram(X, y, kmeans.fit_predict(X), f'{dataset_name} worst score; n_clusters={worst_score_idx}')
-
-    return (best_score, best_score_idx), (worst_score, worst_score_idx)
+        plot_voronoi_diagram(X, None, kmeans.fit_predict(X), f'{dataset_name} worst score; n_clusters={worst_score_idx}')
 
 
 def experiment_one_kmeans():
     # load Iris, Wine, Breast datasets from Scikit, and artificial datasets from CSV
-    dataset_iris = load_iris(return_X_y=True)
-    dataset_wine = load_wine(return_X_y=True)
-    dataset_breast_cancer = load_breast_cancer(return_X_y=True)
+    # dataset_iris = load_iris(return_X_y=True)
+    # dataset_wine = load_wine(return_X_y=True)
+    # dataset_breast_cancer = load_breast_cancer(return_X_y=True)
     dataset_1_1 = load_data("./data/1_1.csv")
     dataset_1_2 = load_data("./data/1_2.csv")
     dataset_1_3 = load_data("./data/1_3.csv")
@@ -138,9 +137,9 @@ def experiment_one_kmeans():
     dataset_2_3 = load_data("./data/2_3.csv")
 
     # silhouette score for all datasets, voronoi for 2D
-    analyze_kmeans_silhouette_score(dataset_iris[0], dataset_iris[1], "iris")
-    analyze_kmeans_silhouette_score(dataset_wine[0], dataset_wine[1], "wine")
-    analyze_kmeans_silhouette_score(dataset_breast_cancer[0], dataset_breast_cancer[1], "breast cancer")
+    # analyze_kmeans_silhouette_score(dataset_iris[0], dataset_iris[1], "iris")
+    # analyze_kmeans_silhouette_score(dataset_wine[0], dataset_wine[1], "wine")
+    # analyze_kmeans_silhouette_score(dataset_breast_cancer[0], dataset_breast_cancer[1], "breast cancer")
     analyze_kmeans_silhouette_score(dataset_1_1[0], dataset_1_1[1], "1_1")
     analyze_kmeans_silhouette_score(dataset_1_2[0], dataset_1_2[1], "1_2")
     analyze_kmeans_silhouette_score(dataset_1_3[0], dataset_1_3[1], "1_3")
@@ -149,4 +148,83 @@ def experiment_one_kmeans():
     analyze_kmeans_silhouette_score(dataset_2_3[0], dataset_2_3[1], "2_3")
 
 
-experiment_one_kmeans()
+def analyze_kmeans_classifier(X, y, dataset_name):
+    adj_rand_scores = [0, 0]
+    homogeneity_scores = [0, 0]
+    completeness_scores = [0, 0]
+    v_measure_05_scores = [0, 0]
+    v_measure_10_scores = [0, 0]
+    v_measure_15_scores = [0, 0]
+
+    X = StandardScaler().fit_transform(X)
+    best_score = -1
+    best_score_idx = -1
+    worst_score = 1
+    worst_score_idx = -1
+
+    for n in range(2, n_clusters_max):
+        kmeans = KMeans(n_clusters=n, random_state=1, n_init="auto")
+        predictions = kmeans.fit_predict(X)
+
+        # calculate all metrics
+        adj_rand = adjusted_rand_score(y, predictions)
+        homogeneity = homogeneity_score(y, predictions)
+        completeness = completeness_score(y, predictions)
+        v_measure_05 = v_measure_score(y, predictions, beta=0.5)
+        v_measure_10 = v_measure_score(y, predictions, beta=1.0)
+        v_measure_15 = v_measure_score(y, predictions, beta=1.5)
+
+        # append to arrays
+        adj_rand_scores.append(adj_rand)
+        homogeneity_scores.append(homogeneity)
+        completeness_scores.append(completeness)
+        v_measure_05_scores.append(v_measure_05)
+        v_measure_10_scores.append(v_measure_10)
+        v_measure_15_scores.append(v_measure_15)
+
+        # get average of all metrics
+        avg_score = np.average([adj_rand, homogeneity, completeness, v_measure_05, v_measure_10, v_measure_15])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            best_score_idx = n
+
+        if avg_score < worst_score:
+            worst_score = avg_score
+            worst_score_idx = n
+
+    print(f'{dataset_name} best average scores: {best_score} for n_clusters={best_score_idx}')
+    print(f'{dataset_name} worst average scores: {worst_score} for n_clusters={worst_score_idx}')
+
+    plt.plot(np.arange(n_clusters_max), adj_rand_scores, label="adjusted rand score")
+    plt.plot(np.arange(n_clusters_max), homogeneity_scores, label="homogeneity score")
+    plt.plot(np.arange(n_clusters_max), completeness_scores, label="completeness score")
+    plt.plot(np.arange(n_clusters_max), v_measure_05_scores, label="V-measure (beta=0.5)")
+    plt.plot(np.arange(n_clusters_max), v_measure_10_scores, label="V-measure (beta=1.0)")
+    plt.plot(np.arange(n_clusters_max), v_measure_15_scores, label="V-measure (beta=1.5)")
+    plt.legend()
+    plt.xlim([2, n_clusters_max - 1])
+    plt.ylabel('silhouette score')
+    plt.xlabel('n_clusters')
+    plt.title(f'For {dataset_name} dataset')
+    plt.show()
+
+    if X.shape[1] == 2:
+        kmeans = KMeans(n_clusters=best_score_idx, random_state=1, n_init="auto")
+        plot_voronoi_diagram(X, y, kmeans.fit_predict(X), f'{dataset_name} best score; n_clusters={best_score_idx}')
+        kmeans = KMeans(n_clusters=worst_score_idx, random_state=1, n_init="auto")
+        plot_voronoi_diagram(X, y, kmeans.fit_predict(X), f'{dataset_name} worst score; n_clusters={worst_score_idx}')
+
+
+def experiment_two_kmeans():
+    dataset_1_1 = load_data("./data/1_1.csv")
+    dataset_1_2 = load_data("./data/1_2.csv")
+    dataset_1_3 = load_data("./data/1_3.csv")
+    dataset_2_1 = load_data("./data/2_1.csv")
+    dataset_2_2 = load_data("./data/2_2.csv")
+    dataset_2_3 = load_data("./data/2_3.csv")
+
+    analyze_kmeans_classifier(dataset_1_1[0], dataset_1_1[1], "1_1")
+
+
+experiment_two_kmeans()
