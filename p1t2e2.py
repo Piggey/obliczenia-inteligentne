c391931944cab9389d0c88b53489f3300f73f20b
arturpietrzak <artur.pietrzak1@outlook.com>
2024-04-15 23:36:48 +0200

    Add experiment two for SVM

--- p1t2e2.py
+++ p1t2e2.py
@@ -139,11 +139,60 @@ def analyze_svm(X_train, X_test, y_train, y_test, dataset_name):
     # plt.close()
     print(f'For dataset {dataset_name} SVM best accuracy = {best_accuracy}; at C = {best_C} or log(C) = {np.log10(best_C)}')
 
-    # show decision boundary for smallest, largest and the best C parameter
+    # show decision boundary and confusion matrix for smallest, largest and the best n_neighbors parameter
+    # smallest
+    classifier = SVC(kernel="rbf", C=analyzed_Cs[0])
+    classifier.fit(X_train, y_train)
+    display = DecisionBoundaryDisplay.from_estimator(classifier, X_test, response_method="predict", alpha=0.5)
+    display.ax_.scatter(X_test[:, 0], X_test[:, 1], c=y_test, edgecolor="k")
+    plt.title(f'DB plot for SVM smallest C; C = {round(analyzed_Cs[1], 2)}; {dataset_name} dataset')
+    plt.show()
+    # plt.savefig(f'{dataset_name}_smallest_db.png')
+    # plt.close()
 
-    # show confusion matrix for smallest, largest and the best C parameter
+    conf_matrix = confusion_matrix(y_test, classifier.predict(X_test), labels=classifier.classes_)
+    display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=classifier.classes_)
+    display.plot()
+    plt.title(f'Confusion matrix for SVM smallest C; C = {round(analyzed_Cs[1], 2)}; {dataset_name} dataset')
+    plt.show()
+    # plt.savefig(f'{dataset_name}_smallest_cm.png')
+    # plt.close()
 
-    pass
+    # largest
+    classifier = SVC(kernel="rbf", C=analyzed_Cs[-1])
+    classifier.fit(X_train, y_train)
+    display = DecisionBoundaryDisplay.from_estimator(classifier, X_test, response_method="predict", alpha=0.5)
+    display.ax_.scatter(X_test[:, 0], X_test[:, 1], c=y_test, edgecolor="k")
+    plt.title(f'DB plot for SVM largest C; C = {round(analyzed_Cs[-1], 2)}; {dataset_name} dataset')
+    plt.show()
+    # plt.savefig(f'{dataset_name}_largest_db.png')
+    # plt.close()
+
+    conf_matrix = confusion_matrix(y_test, classifier.predict(X_test), labels=classifier.classes_)
+    display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=classifier.classes_)
+    display.plot()
+    plt.title(f'Confusion matrix for SVM largest C; C = {round(analyzed_Cs[-1], 2)}; {dataset_name} dataset')
+    plt.show()
+    # plt.savefig(f'{dataset_name}_largest_cm.png')
+    # plt.close()
+
+    # best
+    classifier = SVC(kernel="rbf", C=best_C)
+    classifier.fit(X_train, y_train)
+    display = DecisionBoundaryDisplay.from_estimator(classifier, X_test, response_method="predict", alpha=0.5)
+    display.ax_.scatter(X_test[:, 0], X_test[:, 1], c=y_test, edgecolor="k")
+    plt.title(f'DB plot for SVM best C; C = {round(best_C, 2)}; {dataset_name} dataset')
+    plt.show()
+    # plt.savefig(f'{dataset_name}_best_db.png')
+    # plt.close()
+
+    conf_matrix = confusion_matrix(y_test, classifier.predict(X_test), labels=classifier.classes_)
+    display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=classifier.classes_)
+    display.plot()
+    plt.title(f'Confusion matrix for SVM best C; C = {round(best_C, 2)}; {dataset_name} dataset')
+    plt.show()
+    # plt.savefig(f'{dataset_name}_best_cm.png')
+    # plt.close()
 
 
 def analyze_mlp(X_train, X_test, y_train, y_test, dataset_name):
@@ -169,7 +218,7 @@ def experiment_two():
     # analyze_knn(dataset_2_2[0], dataset_2_2[1], dataset_2_2[2], dataset_2_2[3], "2_2")
     # analyze_knn(dataset_2_3[0], dataset_2_3[1], dataset_2_3[2], dataset_2_3[3], "2_3")
 
-    # analyze_svm(dataset_2_2[0], dataset_2_2[1], dataset_2_2[2], dataset_2_2[3], "2_2")
+    analyze_svm(dataset_2_2[0], dataset_2_2[1], dataset_2_2[2], dataset_2_2[3], "2_2")
     analyze_svm(dataset_2_3[0], dataset_2_3[1], dataset_2_3[2], dataset_2_3[3], "2_3")
 
 
