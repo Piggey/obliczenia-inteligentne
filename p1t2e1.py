08351efa135730e8a567a4687473e9ad269570cd
arturpietrzak <artur.pietrzak1@outlook.com>
2024-04-15 15:44:05 +0200

    Experiment 1 initial commit

---
+++ p1t2e1.py
@@ -0,0 +1,10 @@
+# project 1; task 2; experiment 1;
+
+# plot decision boundary for each dataset, for rbf and linear kernel, for C with best accuracy over training dataset
+def analyze_svm():
+    pass
+
+
+# plot decision boundary for each dataset, for identity, logistic, tanh and relu activation functions, for n_neurons with best accuracy over training dataset
+def analyze_mlp():
+    pass
