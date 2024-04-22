90542499c4b5900f3d18ff31aa3e722864faeca9
arturpietrzak <artur.pietrzak1@outlook.com>
2024-04-22 13:14:16 +0200

    Add all datasets

--- project_2_task_1/p2t1e1.py
+++ project_2_task_1/p2t1e1.py
@@ -1,11 +1,15 @@
 from sklearn.datasets import load_iris, load_wine, load_breast_cancer
 from sklearn.model_selection import train_test_split
 from torch.utils.data import random_split
-
+from torchvision import datasets, transforms
 from project_2_task_1.custom_mlp import CustomDataset
 
 
 def experiment_one():
+    mnist_transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
     dataset_iris = load_iris(return_X_y=True)
     dataset_wine = load_wine(return_X_y=True)
     dataset_breast_cancer = load_breast_cancer(return_X_y=True)
@@ -17,8 +21,12 @@ def experiment_one():
     train_dataset_iris, test_dataset_iris = random_split(dataset_iris, [0.8, 0.2])
     train_dataset_wine, test_dataset_wine = random_split(dataset_wine, [0.8, 0.2])
     train_dataset_breast_cancer, test_dataset_breast_cancer = random_split(dataset_breast_cancer, [0.8, 0.2])
+    train_mnist = datasets.MNIST('../data', train=True, download=True, transform=mnist_transform)
+    test_mnist = datasets.MNIST('../data', train=False, transform=mnist_transform)
 
-    print(train_dataset_iris[0])
+    # TODO: MNIST ekstrakcja - spłaszczenia do wektora 784 elementów
+    # TODO: MNIST 2x ekstrakcja - spłaszczenia do wektora 2 elementów (cech) (po jednym sposobie na osobe)
+    # TODO: MNIST 2x ekstrakcja - spłaszczenia do wektora z małą liczbą elementów (cech) (po jednym sposobie na osobe)
 
 
 experiment_one()
