36b223424d3d71ca485426342aed7ca20ac4309b
arturpietrzak <artur.pietrzak1@outlook.com>
2024-05-13 11:28:42 +0200

    Add functions for experiments

--- project_2_task_2/p2t2e1artur.py
+++ project_2_task_2/p2t2e1artur.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -42,6 +43,47 @@ class ArturCNN(nn.Module):
         x = self.fc2(x)
 
         return F.log_softmax(x, dim=1), two_features
+
+
+def model_accuracy(model, dataloader):
+    model.eval()
+
+    total_correct = 0
+    total_instances = 0
+
+    #  iterating through batches
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            classifications = torch.argmax(model(inputs), dim=1)
+            correct_predictions = sum(classifications == labels).item()
+            total_correct += correct_predictions
+            total_instances += len(inputs)
+
+    return round(total_correct / total_instances, 3)
+
+
+def train_model(model, loss_function, optimizer, epochs, train_dataloader, test_dataloader):
+    for epoch in range(epochs):
+        model.train()
+        loss = -1
+
+        for batch in train_dataloader:
+            inputs, labels = batch
+            outputs = model(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        print(f"epoch {epoch + 1} finished; accuracy {model_accuracy(model, test_dataloader)}; loss {loss}")
+
+
+def analyze_two_features_cnn():
+    pass
+
+
+def analyze_n_features_cnn():
+    pass
 
 
 def experiment_one():
