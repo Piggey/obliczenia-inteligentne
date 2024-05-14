92fb8212b06ea40b635ac5ce69e13720b4f5fceb
Artur <artur.pietrzak1@outlook.com>
2024-05-15 00:27:58 +0200

    Add model saving

--- project_2_task_2/p2t2e1artur.py
+++ project_2_task_2/p2t2e1artur.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from matplotlib import pyplot as plt, cm
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from torch import optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
@@ -18,7 +19,7 @@ class TwoFeaturesArturCNN(nn.Module):
         super(TwoFeaturesArturCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 8, 5)  # 8x28x28
         self.pool1 = nn.MaxPool2d(2, 2)  # 8x12x12
-        self.conv2 = nn.Conv2d(8, 4, 5)  # 16x10x10
+        self.conv2 = nn.Conv2d(8, 4, 5)  # 4x10x10
         self.pool2 = nn.MaxPool2d(2, 2)  # 16x4x4
         self.fc1 = nn.Linear(4 * 4 * 4, 8)
         self.fc2 = nn.Linear(8, 2)
@@ -62,6 +63,23 @@ class ArturCNN(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
+def plot_confusion_matrix_from_dataloader(model, dataloader, classes, is_two_features):
+    y_true = np.array([])
+    y_pred = np.array([])
+
+    for inputs, labels in dataloader:
+        if is_two_features:
+           y_pred = np.append(y_pred, np.argmax(model(inputs)[0].detach().numpy(), axis=1))
+        else:
+            y_pred = np.append(y_pred, np.argmax(model(inputs).detach().numpy(), axis=1))
+        y_true = np.append(y_true, labels)
+
+    conf_matrix = confusion_matrix(y_true, y_pred, labels=np.arange(len(classes)))
+    display = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=classes)
+    display.plot()
+    plt.show()
 
 
 def model_accuracy(model, dataloader, is_two_features):
@@ -110,16 +128,13 @@ def train_model(model, loss_function, optimizer, epochs, train_dataloader, test_
             outputs, two_features = model(inputs)
             for i in range(10):
                 plt.scatter(-999, -999, alpha=1, label=str(i), cmap=cm.tab10)
-            plt.scatter(two_features.detach().numpy()[:, 0], two_features.detach().numpy()[:, 1], c=np.argmax(outputs.detach().numpy(), axis=1)[:], alpha=1, cmap=cm.tab10, s=50, marker="s", edgecolor='none')
-            # plt.scatter(X[:500, 0], X[:500, 1], c=y_true[:500], edgecolors='k', s=20, cmap=cm.tab10)
+            plt.scatter(two_features.detach().numpy()[:, 0], two_features.detach().numpy()[:, 1], c=np.argmax(outputs.detach().numpy(), axis=1)[:], alpha=1, cmap=cm.tab10, s=50, marker="o")
             plt.xlabel('x1')
             plt.ylabel('x2')
             plt.xlim([0, 100])
             plt.ylim([0, 100])
             plt.legend()
             plt.show()
-
-
 
 
 def analyze_two_features_cnn():
@@ -158,6 +173,10 @@ def analyze_two_features_cnn():
     plt.legend()
     plt.show()
 
+    plot_confusion_matrix_from_dataloader(model, test_mnist_dataloader, np.arange(10), True)
+
+    return model
+
 
 def analyze_n_features_cnn():
     train_mnist = datasets.MNIST('../data', train=True, download=True, transform=transform)
@@ -168,15 +187,21 @@ def analyze_n_features_cnn():
     # Create and train model
     model = ArturCNN()
     loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
-    train_model(model, loss_function, optimizer, 20, train_mnist_dataloader, test_mnist_dataloader, False)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_model(model, loss_function, optimizer, 50, train_mnist_dataloader, test_mnist_dataloader, False)
 
-    # TODO: Create multiple models and find the best one
+    plot_confusion_matrix_from_dataloader(model, test_mnist_dataloader, np.arange(10), False)
+
+    return model
+
 
 
 def experiment_one():
-    analyze_two_features_cnn()
-    # analyze_n_features_cnn()
+    two_features_model = analyze_two_features_cnn()
+    torch.save(two_features_model.state_dict(), "two_features_artur.pt")
+
+    # n_features_model = analyze_n_features_cnn()
+    # torch.save(n_features_model.state_dict(), "./models/n_features_artur.pt")
 
 
 experiment_one()
