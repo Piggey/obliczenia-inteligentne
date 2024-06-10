0b29796b13be43402291e3d9db686baebade94d7
Piggey <pepcio3320@gmail.com>
2024-06-10 20:06:34 +0200

    nie ucz

--- project_3/models/breast_cancer/breast_cancer_model.py
+++ project_3/models/breast_cancer/breast_cancer_model.py
@@ -6,25 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
-# Załaduj dane
-cancer = load_breast_cancer()
-X = cancer.data
-y = cancer.target
-
-# Podziel dane na zbiór treningowy i testowy
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Znormalizuj dane
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-# Konwersja danych do tensorów PyTorch
-X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
-y_train_tensor = torch.tensor(y_train, dtype=torch.long)
-X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
-y_test_tensor = torch.tensor(y_test, dtype=torch.long)
-
 # Zbuduj model MLP
 class MLP(nn.Module):
     def __init__(self):
@@ -39,34 +20,56 @@ class MLP(nn.Module):
         x = self.output(x)
         return x
 
-model = MLP()
+if __name__ == '__main__':
+  # Załaduj dane
+  cancer = load_breast_cancer()
+  X = cancer.data
+  y = cancer.target
 
-# Hiperparametry
-learning_rate = 0.01
-num_epochs = 100
+  # Podziel dane na zbiór treningowy i testowy
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Funkcja kosztu i optymalizator
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+  # Znormalizuj dane
+  scaler = StandardScaler()
+  X_train = scaler.fit_transform(X_train)
+  X_test = scaler.transform(X_test)
 
-# Trening
-for epoch in range(num_epochs):
-    optimizer.zero_grad()
-    outputs = model(X_train_tensor)
-    loss = criterion(outputs, y_train_tensor)
-    loss.backward()
-    optimizer.step()
+  # Konwersja danych do tensorów PyTorch
+  X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+  y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+  X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+  y_test_tensor = torch.tensor(y_test, dtype=torch.long)
 
-    if (epoch+1) % 10 == 0:
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
-# Oceń dokładność modelu
-with torch.no_grad():
-    test_outputs = model(X_test_tensor)
-    _, predicted = torch.max(test_outputs, 1)
-    accuracy = accuracy_score(y_test_tensor, predicted)
-    print(f'Accuracy on test set: {accuracy:.4f}')
 
-# Zapisz model do pliku
-torch.save(model.state_dict(), 'project_3/models/breast_cancer/mlp_breast_cancer_model.pth')
-print('Model saved to mlp_breast_cancer_model.pth')
+  model = MLP()
+
+  # Hiperparametry
+  learning_rate = 0.01
+  num_epochs = 100
+
+  # Funkcja kosztu i optymalizator
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+  # Trening
+  for epoch in range(num_epochs):
+      optimizer.zero_grad()
+      outputs = model(X_train_tensor)
+      loss = criterion(outputs, y_train_tensor)
+      loss.backward()
+      optimizer.step()
+
+      if (epoch+1) % 10 == 0:
+          print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+  # Oceń dokładność modelu
+  with torch.no_grad():
+      test_outputs = model(X_test_tensor)
+      _, predicted = torch.max(test_outputs, 1)
+      accuracy = accuracy_score(y_test_tensor, predicted)
+      print(f'Accuracy on test set: {accuracy:.4f}')
+
+  # Zapisz model do pliku
+  torch.save(model.state_dict(), 'project_3/models/breast_cancer/mlp_breast_cancer_model.pth')
+  print('Model saved to mlp_breast_cancer_model.pth')
